75834f2f88a397a5713cfe66c7b6e42d1a9a7e2b
yu <yu_adachi@albert2005.co.jp>
2020-07-26 02:40:33 +0000

    first commit

---
+++ env_website.sh
@@ -0,0 +1,8 @@
+# environment for website_test
+## daily実行前にrepositoryごとの設定されたpythonの環境を再構築し直すための
+### というのも、環境が更新されているかもしれないから。
+### というのも、複数repositoryに対して実行するには異なる環境でも対応しなければならないから。
+python3.7 -m venv website_test
+source website_test/bin/activate
+pip install --upgrade pip
+pip install requests
